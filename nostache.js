9f6e9ad9b07cc74d0a39dab9ca3eb07a0c27be3e
dandavis <rndme@users.noreply.github.com>
2016-01-02 02:31:43 -0600

    added extensive comments, make RXs more explicit

--- nostache.js
+++ nostache.js
@@ -1,19 +1,19 @@
 // nostache.js by dandavis [CCBY4]
-
 var nostache=(function() {
-	var rxComments = /\{\{\![^}]*\}\}/g,
-		rxIndex = /\$\{INDEX\}/g,
-		rxImport = /\{\{>([^}]+)\}\}/g,
-		rxSep = /\$\{SEP\}([^$]+)\$\{\/SEP\}/g,
-		rxBraces = /\{\{([^\}]+)\}\}/g,
-		rxElse = /\{\{\!([\w\.]+)\}\}/g,
-		rxNot = /([\^])([^}]+)\}(.+?)\$\{\/\2/g,
-		rxIf = /([#])([^}]+)\}(.+?)\$\{\/\2/g,
-		rxLoop = /([\.])([^}]+)\}([\w\W]+?)\$\{\/\2/g,
-		rxRazor=/(\W)@([#\^!\/\|\.\)\(]?[\w\.$|]+)/g,
-		rxCarrot = /\$\{\.\}/g;
+  // define the syntax of the templates as RegExps:
+	var rxImports = /\{\{>([^}]+)\}\}/g,			// {{>...}}
+		rxIndex = /\$\{INDEX\}/g, 				// ${INDEX}
+		rxRazor=/(\W)@([#\^!\/\|\.\)\(]?[\w\.$|]+)/g,	// @[#^!/.)(]*ab.c
+		rxElse = /\{\{\!([\w\.]+)\}\}/g,		//	{{!abc}}
+		rxSep = /\$\{SEP\}([^$]+)\$\{\/SEP\}/g, // ${SEP}...${/SEP}
+		rxComments = /\{\{\![^}]*\}\}/g,		// {{!...}}
+		rxBraces = /\{\{([^\}]+)\}\}/g,			// {{...}}
+		rxNot = /\$\{([\^])([^}]+)\}(.+?)\$\{\/\2/g,	// ${^...}		
+		rxIf = /\$\{([#])([^}]+)\}(.+?)\$\{\/\2/g,		// ${#...}
+		rxLoop = /([\.])([^}]+)\}([\w\W]+?)\$\{\/\2/g,	// ?????
+		rxCarrot = /\$\{\.\}/g;	// ${.}
 
-
+  	// the internal string to ES6 converter and executer, psuedo-recursive on loops
 	function _tmp(s, ob, index, inner, c) {
 		var z, ss = s
 		.replace(rxIndex, index + 1) // turn INDEX keyword into numeric literal
@@ -22,23 +22,28 @@ var nostache=(function() {
 		.replace(rxSep, c&&(index<c.length-1)?"$1":"")
 		.replace(rxComments, "") // strip comment blocks
 		.replace(rxBraces, "${$1}") // turn brace expressions into template string literals
-		.replace(rxNot, "!($2)?\"$3\":''") // condense NOT block into template expression
-		.replace(rxIf, "$2?\"$3\":''") // condense IF block into template expression
+		.replace(rxNot, "${!($2)?\"$3\":''") // condense NOT block into template expression
+		.replace(rxIf, "${$2?\"$3\":''") // condense IF block into template expression
 		.replace(rxLoop, function(j,k,a,b){return "("+a+").map((a,b,c)=>_tmp.call(this,"+JSON.stringify(b)+",a,b,true,c),this).join('')";}) // condense loop block into template expression
 		.replace(rxCarrot, "${ob}") // turn carrot marker into template expression
-		, junk=console.info(ss, this), 
+		junk=console.log(ss),
 		rez = Function("_tmp, ob", "try{with(ob)return `" + ss + "`}catch(y){return  y.constructor.name + '::' + y.message;}");
 
+		// if internally called, return composited  string using context (not whole data):
 		if(inner) return rez.call(this, _tmp, ob);
-		return function(data) {
-			return rez.call(this, _tmp, data);
-		};
+	  
+		// return a render function bound to the template internal renderer:
+		return function(data) {	return rez.call(this, _tmp, data);};
 	}
 
-    return function tmp(s, data, partials){
-	var that= partials || this || {};
-	s=s.replace(rxImport, function(j,p){return that[p];});
-	return data ? _tmp.call(this,s).call(this, data) : _tmp(s);
+    return function tmp(strTemplate, data, imports){	// the nostache function, accepts a string, data, and imports
+		// define imports from explictly-passed object, _this_, or a blank object
+		var partials= imports || this || {};
+		// run imports by replacing tokens with values from the imports object:
+		strTemplate=strTemplate.replace(rxImports, function(j,p){ return partials[p]; });
+		
+		return data ? // return a render function, or if given data also, a composited string result:
+			_tmp.call(this, strTemplate).call(this, data) : 
+			_tmp(strTemplate) ;
     };
-
 }());
