2eb25c79f0225842bac797ee543a044b9a1bb8f9
dandavis <rndme@users.noreply.github.com>
2016-01-05 22:45:44 -0600

    reformat code

--- nostache.js
+++ nostache.js
@@ -1,4 +1,4 @@
-// nostache.js by dandavis [CCBY4]  https://github.com/rndme/nostache
+  // nostache.js by dandavis [CCBY4]  https://github.com/rndme/nostache
 var nostache=(function() {
   // define syntax of the templates as RegExps:			approximation/eg
 	var rxImports = /\{\{>([\w\W]+?)\}\}/g,			// {{>...}}
@@ -12,10 +12,11 @@ var nostache=(function() {
 		rxIf = /\$\{([#])([\w\W]+?)\}(.+?)\$\{\/\2/g,	// ${# ...}...${$1}
 		rxObj = /\$\{(\.)([\w.]+?):([\w.]+?)\}([\w\W]+?)\$\{\/\2:\3/g,	// ${.:. ...}...${$1}
 		rxLoop = /\$\{(\.)([\w\W]+?)\}([\w\W]+?)\$\{\/\2/g,	// ${. ...}...${$1}	
-		rxCarrot = /\$\{\.\}/g;	// ${.}
-
-	function _tmp(s, ob, index, inner, c, data, KEY) {	// string to ES6 converter/executer
-		var ss = s					// string FunctionBody of the dynamic rednerer
+		rxCarrot = /\$\{\.\}/g,	// ${.}
+		J=JSON.stringify;
+		
+	function _tmp(strTemplate, ob, index, inner, c, data, KEY) {	// string to ES6 converter/executer
+		s = strTemplate				// string FunctionBody of the dynamic rednerer
 		.replace(rxIndex, index + 1) 			// turn INDEX keyword into numeric literal
 		.replace(rxRazor, "$1{{$2}}") 			// convert Razor to normal syntax
 		.replace(rxElse, "{{/$1}}{{^$1}}") 		// turn ELSE expressions into conditional syntax
@@ -25,16 +26,13 @@ var nostache=(function() {
 		.replace(rxNot, "${!($2)?\"$3\":''")		// condense NOT block into template expression
 		.replace(rxIf, "${$2?\"$3\":''")			// condense IF block into template expression
 		// condense array+object loops into template expressions:
-		.replace(rxObj, (j,k,a,b,c)=> "${Object.keys("+a+").map(function(a,b,c){return _tmp.call(this,"+JSON.stringify(c)+",this[a]||a,b,true,c,__,"+JSON.stringify(b)+");},ob["+JSON.stringify(a)+"]).join('')") // object 
-		.replace(rxLoop, (j,k,a,b)=> "${("+a+").map((a,b,c)=>_tmp.call(this,"+JSON.stringify(b)+",a,b,true,c,__),this).join('')") // array
-		.replace(rxCarrot, "${ob}"),			// turn carrot marker into template expression
-		rez = Function("_tmp, ob, __, "+KEY, "with(ob)return `" + ss + "`;");	// build string output renderer function
-
-		// if internally called, return composited  string using context (not whole data):
-		if(inner) return rez.call(this, _tmp, ob, data, KEY);
-
-		// return a render function bound to the template internal renderer:
-		return function(data) {	return rez.call(this, _tmp, data, data, KEY);};
+		.replace(rxObj, (j,k,a,b,c)=> "${Object.keys("+a+").map(function(a,b,c){return _tmp.call(this,"+J(c)+",this[a]||a,b,true,c,__,"+J(b)+");},ob["+J(a)+"]).join('')") // object 
+		.replace(rxLoop, (j,k,a,b)=> "${("+a+").map((a,b,c)=>_tmp.call(this,"+J(b)+",a,b,true,c,__),this).join('')") // array
+		.replace(rxCarrot, "${ob}");			// turn carrot marker into template expression
+		
+		var rez = Function("_tmp, ob, __, "+KEY, "with(ob)return `" + s + "`;");	// build string output renderer function
+		if(inner) return rez.call(this, _tmp, ob, data, KEY);// if internally called, returns composited  string using context (not whole data)
+		return function(data) { return rez.call(this, _tmp, data, data, KEY); }; // returns a render function bound to the template internal renderer
 	}
 
     return function tmp(strTemplate, data, imports){	// the nostache function, accepts a string, data, and imports
@@ -48,3 +46,4 @@ var nostache=(function() {
 			_tmp(strTemplate) ;
     };
 }());
+  
