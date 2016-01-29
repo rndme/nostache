859a0cbfce44f528c9c9724c9af3f503205248b4
dandavis <rndme@users.noreply.github.com>
2016-01-29 01:39:16 -0600

    hint optimizations, shadow self, SEPs, imports

--- ustache2.js
+++ ustache2.js
@@ -1,32 +1,43 @@
+
+  
 // ustache 2nd gen platform preview [CCBY4]-dandavis
-
-function Template(objData, strTemplate){ //returns a new template function to match the data shape
-
-      // replace else expressions: {{|path}} turns into {{/path}}{{^path}}
-  strTemplate=strTemplate.replace(/\{\{\|([\w\W]+?)\}\}/g, function(j,k){
+function Template(strTemplate, objData, objImports, blnNow){ //returns a new template function to match the data shape
+	var s=strTemplate, immed=blnNow||arguments.length==3 && objImports==true;
+	function rep(hint, rx, replacment){	return s= (s.indexOf(hint)!==-1) ? s.replace(rx, replacment) : s, rep;	}
+  
+   	// run imports by replacing tokens with values from the imports object:
+ rep("{{>",/\{\{>\s*([\w\W]+?)\s*\}\}/g, function(j,k){
+          return objImports[k];
+ })  	// replace else expressions: {{|path}} turns into {{/path}}{{^path}}
+ ("{{|",/\{\{\|([\w\W]+?)\}\}/g, function(j,k){
            return "{{/"+k+"}}{{^"+k+"}}";
-  })  // replace array iterators:  {{#arr}} ${self.bold()} {{/arr}}
-  strTemplate=strTemplate.replace(/\{\{#([\w.]+)\}\}([\w\W]+?)(\{\{\/\1\}\})/g, function(j,k,v){
-           return "${[].map.call("+k+"||[], (self,INDEX)=>\` "+v+"\`,this).filter(notNull).join('')}";
-  })  // replace conditionals:  {{#x>21}}ADULT{{/x>21}}
- .replace(/\{\{\?([\w\W]+?)\}\}([\w\W]+?)(\{\{\/\1\}\})/g, function(j,k,v){
+ })  // replace SEP sections
+ ("{{SEP}}",/\{\{SEP\}\}([\w\W]+?)\{\{\/SEP\}\}/g, function(j,v){
+           return "${ALL.length!==INDEX?`"+v+"`:''}";
+ })  // replace array iterators:  {{#arr}} ${self.bold()} {{/arr}}
+ ("{{#",/\{\{#([\w.]+)\}\}([\w\W]+?)(\{\{\/\1\}\})/g, function(j,k,v){
+	return 	("${[].map.call("+k+"||[], function(self,INDEX,ALL){"+(v.indexOf("self") === -1 ? "with(self)" : "")+
+			 "return ++INDEX&&\`"+v+"\`},this).filter(ok).join('')}");
+ })  // replace conditionals:  {{#x>21}}ADULT{{/x>21}}
+ ("{{?",/\{\{\?([\w\W]+?)\}\}([\w\W]+?)(\{\{\/\1\}\})/g, function(j,k,v){
            return "${"+k+"?`"+v+"`:''}";
-  })  // replace negative conditionals:  {{^x>21}}ADULT{{/x>21}}
- .replace(/\{\{\^([\w\W]+?)\}\}([\w\W]+?)(\{\{\/\1\}\})/g, function(j,k,v){
+ })  // replace negative conditionals:  {{^x>21}}ADULT{{/x>21}}
+ ("{{^",/\{\{\^([\w\W]+?)\}\}([\w\W]+?)(\{\{\/\1\}\})/g, function(j,k,v){
            return "${!("+k+")?`"+v+"`:''}";
-  })  // replace injections:  {{=x}}
-  .replace(/\{\{=([\w\W]+?)\}\}/g, function(j,k){
+ })  // replace dots:  {{.}}
+ ("{{.}}",/\{\{\.\}\}/g, "${self}"
+ )   // replace injections:  {{=x}}
+ ("{{",/\{\{=?([\w\W]+?)\}\}/g, function(j,k){
            return "${"+k+"}";
-  })  // replace razors:  @x.y 
-  .replace(/(\W)@([\w.()'",]+)([\s\b\W])/g, function(j,h,b,t){
+ })  // replace razors:  @x.y 
+ ("@",/(\W)@([\w.()'",]+)([\s\b\W]|\b)/g, function(j,h,b,t){
            return h+"${"+b+"}"+(t||"");
-  });
+ });
 
-
-  return  Function("__", "  var notNull=x=>x!=null, " + 
+  var fn= Function("__", "var self=__,ok=x=>x!=null, " + 
          Object.keys(objData)
-           .filter(a=>strTemplate.indexOf(a)!==-1) // optimize by eliminating shadows for un-used vars
-           .map(a=> a+"=__."+a ) // assign all used keys in objData to vars
-           .join(",\n   ") +
-          ";\n return `"+strTemplate+"`;"); // wrap template string around template contents.
+           .filter(a=>s.indexOf(a)!==-1) // optimize by eliminating shadows for un-used vars
+           .map(a=> a+"=__."+a )+ // assign all used keys in objData to vars
+          ";\n return `"+s+"`;"); // wrap template string around template contents.
+  return immed?fn.call(this, objData):fn;
 }
